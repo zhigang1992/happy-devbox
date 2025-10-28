a731ae2e2befb7bce1b410fbdd3dc7973d387611
Ryan Newton <rrnewton@gmail.com>
2025-10-28 05:53:19 -0700

    commit to main:

--- .devcontainer/commit_to_main.sh
+++ .devcontainer/commit_to_main.sh
@@ -3,7 +3,8 @@ set -xe
 
 BRANCH=$(git branch --show-current)
 git stash && git checkout main && git stash pop
-git commit -am "$*"
+git commit -am "commit to main: $*"
 git push origin main
 git checkout "$BRANCH"
-git pull origin main
+git merge main
+# git pull origin main

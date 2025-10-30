1655ec82c801bed18b6bf0fdceef5389bd7e38ab
Ryan Newton <rrnewton@gmail.com>
2025-10-30 13:22:42 -0400

    commit to main script

--- .devcontainer/commit_to_main.sh
+++ .devcontainer/commit_to_main.sh
@@ -4,7 +4,9 @@ set -xe
 BRANCH=$(git branch --show-current)
 git stash && git checkout main && git stash pop
 git commit -am "commit to main: $*"
-git push origin main
+
+(git pull origin main && git push origin main) || echo "Could not PUSH main changes."
+
 git checkout "$BRANCH"
 git merge main --no-edit
 # git pull origin main

1de043296afb1b55d1ec3ad5cd782198f86d2438
Ryan Newton + Claude <rrnewton@gmail.com>
2025-11-30 23:32:28 +0000

    Switch of dynamic happy server as it isn't working

--- env_setup.sh
+++ env_setup.sh
@@ -1,7 +1,8 @@
 export HAPPY_HOME_DIR=~/.happy
 export HAPPY_WEBAPP_URL=http://localhost:8081
-export HAPPY_SERVER_URL=dynamic
-# export HAPPY_SERVER_URL=http://localhost:3005
+
+# export HAPPY_SERVER_URL=dynamic
+export HAPPY_SERVER_URL=http://localhost:3005
 
 happy_repo=`pwd`
 alias hap="${happy_repo}/happy-cli/bin/happy.mjs"
